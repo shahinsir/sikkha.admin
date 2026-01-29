7e5eb7617951d5d71767775737721996fc99de79
shahinsir <alamshahinur242@gmail.com>
2026-01-29 14:10:01 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'shikkha-admin-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://i.ibb.co/wFnz89n6/icon-192x192.png',
+  'https://i.ibb.co/XGNQ8V4/icon-180x180.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('✅ Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch events
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
